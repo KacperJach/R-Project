a3af06e32d9169cea94de2f9e6f709963e7deae5
KacperJach <56155255+KacperJach@users.noreply.github.com>
2021-03-21 15:19:41 +0100

    Add files via upload

--- MSPROJEKT Cukier/main.R
+++ MSPROJEKT Cukier/main.R
@@ -5,5 +5,12 @@ wielkopolskie <-read.table("dane2.txt", quote="\"", comment.char="")
 
 source("Functions.R")
 zad1_sz(lubuskie$V1)
+zad1_roz(lubuskie$V1)
+zad1_sz(wielkopolskie$V1)
 zad1_roz(wielkopolskie$V1)
 
+zad2(lubuskie$V1)
+zad2(wielkopolskie$V1)
+
+zad3(lubuskie$V1, 0.05)
+zad3(wielkopolskie$V1, 0.05)
